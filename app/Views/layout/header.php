9aa8e4bb9d733205c65f0ea90c21ccd4c24f6a45
dodo-selarassolusindo <dodo.a@selarassolusindo.com>
2022-12-24 11:19:02 +0700

    .

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -5,10 +5,10 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= 'ADEL CCG - ' . esc($title) ?></title>
-    <meta name="description" content="Garment Factory Process">
-    <meta name="keyword" content="garment,factory,khonkaen,ocomshop">
-    <meta name="author" content="ocomshop">
+    <title><?= 'Accounting v1.0 - ' . esc($title) ?></title>
+    <meta name="description" content="Accounting v1.0">
+    <meta name="keyword" content="selaras,solusindo,selusin,accounting,aplikasi,akuntansi">
+    <meta name="author" content="selaras solusindo">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= csrf_meta()?>
@@ -21,15 +21,15 @@
     <link rel="stylesheet" href="<?= base_url('asset/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?= base_url('asset/css/adminlte.min.css') ?>">
-     <!--<link rel="stylesheet" href="<?php /* echo base_url('asset/css/rtl/adminlte.rtl.min.css') */ ?>"> --> 
-	 
+     <!--<link rel="stylesheet" href="<?php /* echo base_url('asset/css/rtl/adminlte.rtl.min.css') */ ?>"> -->
+
     <!-- SweetAlert2 Bootstrap or Dark -->
     <link rel="stylesheet" href="<?= base_url('asset/css/sweetalert2-dark.min.css') ?>">
     <!-- DataTables -->
  	<link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/DataTables-1.11.3/css/dataTables.bootstrap5.min.css') ;?>">
-	<link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/Responsive-2.2.9/css/responsive.bootstrap5.min.css') ;?>"> 
+	<link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/Responsive-2.2.9/css/responsive.bootstrap5.min.css') ;?>">
 
     <link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/StateRestore-1.1.1/css/stateRestore.bootstrap5.min.css') ?>">
     <!-- Dark style -->
-    <!--<link rel="stylesheet" href="<?php /* echo base_url('asset/css/dark/adminlte-dark-addon.min.css')*/ ?>">  --> 
+    <!--<link rel="stylesheet" href="<?php /* echo base_url('asset/css/dark/adminlte-dark-addon.min.css')*/ ?>">  -->
 </head>
