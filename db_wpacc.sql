191cdc1ee54dea7b547faa329b16853b77049f00
dodo-selarassolusindo <dodo.a@selarassolusindo.com>
2022-12-25 19:07:15 +0700

    .

--- db_wpacc.sql
+++ db_wpacc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2022 at 06:50 AM
+-- Generation Time: Dec 25, 2022 at 01:06 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
